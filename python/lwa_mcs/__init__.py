141373dcfe18a131935a94daac7d8eebaac65b9c
Jayce Dowell <jayce.dowell@gmail.com>
2019-11-04 20:25:45 +0000

    Added a get_queue() function to lwa_mcs.exc.  Updated various bits of documentation.

--- python/lwa_mcs/__init__.py
+++ python/lwa_mcs/__init__.py
@@ -1,1 +1,8 @@
-import sch, exc, mib
+"""
+Module to help interface with the LWA MCS software.
+"""
+
+__version__ = "0.1"
+__revision__ = "$Rev$"
+
+import sch, exc, mib, utils
--- python/lwa_mcs/exc.py
+++ python/lwa_mcs/exc.py
@@ -4,16 +4,19 @@
 Module for controlling MCS executive via UDP packets.
 """
 
+import os
 import time
 import socket
 import struct
 import subprocess
+from datetime import datetime, timedelta
 
 from lwa_mcs.config import ADDRESSES, SOCKET_TIMEOUT, EXC_COMMANDS
+from lwa_mcs.utils import mjdmpm_to_datetime
 
 __version__ = "0.1"
 __revision__ = "$Rev$"
-__all__ = ['COMMAND_STRUCT', 'get_pids', 'is_running', 'send_command']
+__all__ = ['COMMAND_STRUCT', 'get_pids', 'is_running', 'send_command', 'get_queue']
 
 
 COMMAND_STRUCT = struct.Struct('i256s')
@@ -78,3 +81,50 @@ def send_command(cmd, data=""):
     time.sleep(0.2)
     
     return False if response[0] < EXC_COMMANDS['NUL'] else True
+
+
+def get_queue():
+    """
+    Read in the current observation queue and return its contents as a 
+    dictionary.  The dictionary is keyed using a two-element tuple of
+    (project ID, session ID) and the values are three-element tuples of
+    (beam, start datetime, stop datetime).
+    """
+    
+    queue = {}
+    
+    # Open the 'mesq.dat' file for parsing
+    try:
+        fh = open(os.path.join(TP_PATH, 'mbox', 'mesq.dat'), 'r')
+        for line in fh:
+            if line.find('S PROJ     SESS B    MJD       MPM       DUR NOBS C') != -1:
+                continue
+                
+            ## Parse the line
+            fields = line.split(None, 10)
+            if len(fields) != 11:
+                continue
+            pID = fields[2]
+            sID = int(fields[3], 10)
+            beam = int(fields[4], 10)
+            mjd, mpm = int(fields[5], 10), int(fields[6], 10)
+            dur = int(fields[7], 10)
+            nobs = int(fields[8], 10)
+            if nobs == 0:
+                continue
+                
+            ## Manipulate the time
+            dStart = mjdmpm_to_datetime(mjd, mpm)
+            dStop  = dStart + timedelta(seconds=int(dur/1000), microseconds=int((dur*1e3) % 1000000))
+            
+            ## Pad to deal with session starts and stops
+            dStart -= timedelta(seconds=6)
+            dStop  += timedelta(seconds=6)
+            
+            ## Append
+            queue[(pID,sID)] = (beam,dStart,dStop)
+            
+    except IOError as e:
+        raise RuntimeError("Cannot parse the 'mesq.dat' file")
+        
+    return queue
