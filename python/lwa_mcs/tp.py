577d961493417090c44c4e2e8585b373ae9745e9
jaycedowell <jayce.dowell@gmail.com>
2020-04-07 16:00:11 -0600

    Added options for writing log and error information generated by tpss in tp.schedule_sdfs() to files.  Added a new tp.get_completed_tarballs() function to help gather together completed tarballs.

--- python/lwa_mcs/tp.py
+++ python/lwa_mcs/tp.py
@@ -4,14 +4,17 @@
 Module for intefacing with MCS task processor.
 """
 
+import os
+import glob
 import time
+import warnings
 import subprocess
 
 from lwa_mcs.config import TP_PATH
 from lwa_mcs.exc import parse_queue
 
-__version__ = "0.1"
-__all__ = ['schedule_sdfs']
+__version__ = "0.2"
+__all__ = ['schedule_sdfs', 'get_completed_metadata']
 
 
 def _get_sdf_id(filename):
@@ -34,17 +37,57 @@ def _get_sdf_id(filename):
     return pid, sid
 
 
-def schedule_sdfs(filenames, max_retries=10):
+def schedule_sdfs(filenames, max_retries=5, logfile=None, errorfile=None):
     # Figure out the input
-    if isinstance(fileanmes, str):
+    if not isinstance(filenames, list):
         filenames = [filenames,]
+        
+    # Deal with the logging
+    ## stdout
+    log_is_string = False
+    if type(logfile) == str:
+        try:
+            fh = open(logfile, 'a')
+            logfile = fh
+            log_is_string = True
+        except IOError as e:
+            warnings.warn("Could not open logfile '%s' for appending: %s" % (logfile, str(e)), 
+                          warnings.RuntimeWarning)
+            logfile = None
+    elif type(logfile) == file:
+        pass
+    elif logfile is None:
+        pass
+    else:
+        warnings.warn("logfile is of unknown type '%s'" % str(type(logfile))), 
+                      warings.RuntimeWarning)
+        logfile = None
+    ## stderr
+    err_is_string = False
+    if type(errorfile) == str:
+        try:
+            fh = open(errorfile, 'a')
+            errorfile = fh
+            err_is_string = True
+        except IOError as e:
+            warnings.warn("Could not open errorfile '%s' for appending: %s" % (logfile, str(e)), 
+                          warnings.RuntimeWarning)
+            errorfile = None
+    elif type(errorfile) == file:
+        pass
+    elif errorfile is None:
+        pass
+    else:
+        warnings.warn("errorfile is of unknown type '%s'" % str(type(logfile))), 
+                      warings.RuntimeWarning)
+        errorfile = None
         
     # Submit the files
     ids = {}
     for filename in filenames:
         ids[_get_sdf_id(filename)] = filename
         tpss = subprocess.Popen(['./tpss', filename, '5', '0', 'mbox'], 
-                                cwd=TP_PATH, stdin=None, stdout=None)
+                                cwd=TP_PATH, stdin=logfile, stdout=errorfile)
         tpss.wait()
         time.sleep(0.5)
         
@@ -69,13 +112,34 @@ def schedule_sdfs(filenames, max_retries=10):
             ### Resubmit what is missing
             for filename in missing_files:
                 tpss = subprocess.Popen(['./tpss', filename, '5', '0', 'mbox'], 
-                                        cwd=TP_PATH, stdin=None, stdout=None)
+                                        cwd=TP_PATH, stdin=logfile, stdout=errorfile)
                 tpss.wait()
                 time.sleep(0.5)
                 
             ### Update the retry counter, giving up as needed
             counter += 1
             if counter > max_retries:
+                if log_is_string:
+                    logfile.close()
+                if err_is_string:
+                    errorfile.close()
                 raise RuntimeError("Cannot schedule all SDFs after %i attempts" % max_retries)
                 
+    if log_is_string:
+        logfile.close()
+    if err_is_string:
+        errorfile.close()
+        
     return True
+
+
+def get_completed_metadata():
+    """
+    Return a list of metadata tarballs for completed observations.
+    """
+    
+    tarballs = glob.glob(os.path.join(TP_PATH, 'mbox', '*.tgz'))
+    # Make sure the files are actually ready by being more than 10 seconds old
+    tarballs = list(filter(lambda x: (time.time() - os.path.getmtime(x)) > 10, tarballs))
+    tarballs.sort()
+    return tarballs
