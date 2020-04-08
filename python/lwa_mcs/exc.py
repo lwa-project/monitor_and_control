3e58d7cca679a7a7087a0e722c173a8a7ff141d6
Jayce Dowell <jayce.dowell@gmail.com>
2020-04-07 19:57:54 -0600

    Fixed a bug in exc.cancel_observations that would have caused it to delete metadata when it shouldn't.  Added a sleep before deleteing the metadata to make sure the file is in place.

--- python/lwa_mcs/exc.py
+++ python/lwa_mcs/exc.py
@@ -130,15 +130,18 @@ def cancel_observation(project_id, session_id, stop_dr=True, remove_metadata=Tru
     if not cancelled:
         raise RuntimError("Cannot cancel observation")
     else:
+        time.sleep(0.5)
+
         if is_active:
             if stop_dr:
                 ## Also stop the data recorder
                 tag = send_subsystem_command("DR%i" % beam, "RPT" "OP-TAG")
                 stopped = send_subsystem_command("DR%i" % beam, "STP", tag)
+        else:
             if remove_metadata:
                 try:
                     os.unlink(os.path.join(TP_PATH, 'mbox', "%s_%04i.tgz" % (project_id, session_id)))
-                except OSError:
+                except OSError as e:
                     pass
                     
     return True
