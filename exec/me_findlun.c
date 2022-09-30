b31d427acaaa618a2c7760d18bd9e2cee868fcd1
jaycedowell <jaycedowell@gmail.com>
2022-09-30 14:08:01 -0600

    Add in the actual Moon finding routines.

---
+++ exec/me_findlun.c
@@ -0,0 +1,70 @@
+// me_findlun.c: J. Dowell, UNM, 2022 Sep 30
+// ---
+// COMPILE: intended to be called from another program which it is compiled into
+// ---
+// COMMAND LINE: N/A
+// ---
+// REQUIRES: 
+//   
+// Find RA and DEC of Jupiter
+// See end of this file for history.
+
+/* The codes below are ported from Xephem */
+#include "ephem_astro.h"  /* mostly gutted, leaving only stuff needed for others */
+
+void me_findlun(
+                 long int mjd, /* (input) modified julian date */
+                 long int mpm, /* (input) milliseconds past UTC midnight */ 
+                 float *ra,    /* (output) [h] RA */
+                 float *dec,   /* (output) [deg] dec */
+                 float *dist   /* (output) [AU] distance from Earth */
+                ) {
+
+  double JD, H, JD0;
+  double ret[6]; /* this is how vsop87() returns output */
+  double L,  B,  R, msp, mdp;
+  double L0, B0, R0;
+  double x,y,z;
+  double x0,y0,z0;
+  double lambda, beta, rho;
+  double lambdaHelio, betaHelio, rhoHelio;
+  double lambdaSun, rhoSun, betaSun;
+  double dRA, dDec;
+
+  /* Get JD from mjd/mpm */
+  JD0 = ((double)mjd) + 2400000.5;     /* ref: http://tycho.usno.navy.mil/mjd.html */
+  H   = ((double)mpm)/(3600.0*1000.0); /* mpm in hours */
+  JD = JD0 + H/24.0; /* days */
+
+  /* Get L, B, R for the Moon so we can figure out how far away it is */
+  moon ( JD-MJD0, &L, &B, &R, &msp, &mdp);
+  
+  /* Correct L, B, and R for the light travel time by looking back in time a bit*/
+  moon ( JD-MJD0-R/173.144633, &L, &B, &R, &msp, &mdp);
+  //printf("me_findlun(): L =%lf rad, B =%lf rad, R =%lf AU\n",L,B,R);
+  
+  /* to equatorial coordinates */
+  ecl_eq(JD-MJD0, B, L, &dRA, &dDec);
+  
+  /* Apply nutation */
+  nut_eq(JD-MJD0, &dRA, &dDec);
+  
+  /* Back to floats */
+  *ra = (float) radhr(dRA);
+  *dec = (float) raddeg(dDec);
+  *dist = (float) R;
+
+  return;
+  } /* me_findlun */
+
+// me_findlun.c: J. Dowell, UNM, 2022 Sep 30
+//  -- initial version, using me_getjov.c as a starting point 
+// me_findjov.c: J. Dowell, UNM, 2015 Sep 1
+//  -- changed the call so that the distance to Jupiter in AU is also returned
+// me_findjov.c: J. Dowell, UNM, 2015 Aug 31
+//   -- updated to do everything through XEphem
+// me_findjov.c: S.W. Ellingson, Virginia Tech, 2012 Oct 07
+//   -- initial version, using me_getaltaz.c as a starting point 
+// me_getaltaz.c: S.W. Ellingson, Virginia Tech, 2012 Jan 21
+//   -- corrections to algorithm 
+// me_getaltaz.c: S.W. Ellingson, Virginia Tech, 2011 May 05
---
+++ exec/me_findlun_x.c
@@ -0,0 +1,72 @@
+// me_findlun_x.c: S.W. Ellingson, Virginia Tech, 2012 Oct 07
+// ---
+// COMPILE: gcc -o me_findlun_x me_findlun_x.c -lm
+// ---
+// COMMAND LINE: 
+// ---
+// EXAMPLE:
+// $ ./me_findlun_x ... (see message below)
+// ---
+// REQUIRES: 
+//   me_findlun.c
+// ---
+// Wrapper used to test me_findlun() 
+// See end of this file for history.
+
+#include <math.h>
+#include <stdio.h>
+
+#include "me_astro.h"
+
+int main ( int narg, char *argv[] ) {
+
+  long int mjd; /* (input) mean julian date */
+  long int mpm; /* (input) milliseconds past UTC midnight */ 
+  float ra;     /* (output) [h] RA */
+  float dec;    /* (output) [deg] dec */
+  float dist;   /* (output) [AU] distance from Earth */
+  float m, s;
+ 
+  /* Parse command line */
+  if (narg>1) sscanf(argv[1],"%ld",&mjd);
+  if (narg>2) sscanf(argv[2],"%ld",&mpm);   
+  if (narg<3) {
+    printf("Useage:\n");
+    printf("me_findlun_x <mjd> <mpm>\n");
+    printf("  <mjd>: integer MJD\n");
+    printf("  <mpm>: integer milliseconds past UTC midnight\n");
+    return;
+    }
+ 
+  me_findlun(
+              mjd,  /* (input) mean julian date */
+              mpm,  /* (input) milliseconds past UTC midnight */ 
+              &ra,  /* (output) [h] calculated RA */
+              &dec, /* (output) [deg] calculated dec */ 
+              &dist /* (output) [AU] distance from Earth */
+             );
+
+  m = (ra - floor(ra))*60.0;
+  s = (m - floor(m))*60.0;
+  printf("RA   = %8.3f h   =  %02.0f h %02.0f m %05.2f s\n",ra, floor(ra), floor(m), s);
+  m = (fabs(dec)-floor(fabs(dec)))*60.0;
+  s = (m - floor(m))*60.0;
+  printf("dec  = %8.3f deg = %+03.0f d %02.0f m %04.1f s\n",dec, (dec/fabs(dec))*floor(fabs(dec)), floor(m), s);
+  printf("dist = %8.6f AU\n", dist);
+
+  return 0;
+  } /* main() */
+
+//==================================================================================
+//=== HISTORY ======================================================================
+//==================================================================================
+// me_findlun_x.c: J. Dowewll, UNM, 2022 Sep 30
+//   .1: Initial version
+// me_findjov_x.c: S.W. Ellingson, Virginia Tech, 2012 Oct 07
+//   .1: Initial version
+// me_getaltazx.c: S.W. Ellingson, Virginia Tech, 2011 May 01
+//   .1: Initial version
+
+//==================================================================================
+//=== BELOW THIS LINE IS SCRATCH ===================================================
+//==================================================================================
