ffbc253187e407fbe5e3c30447f6a2311b3b0ecb
jaycedowell <jaycedowell@gmail.com>
2022-10-11 16:18:23 -0600

    Updated for the SOFA library.

--- exec/me_precess.c
+++ exec/me_precess.c
@@ -9,8 +9,8 @@
 // Precesses a RA/Dec coordinate pair from J2000.0 to the current epoch.  Also, 
 // apply corrections for nutation and aberration to the coordinates.
 
-/* The codes below are ported from Xephem */
-#include "ephem_astro.h"  /* mostly gutted, leaving only stuff needed for others */
+#include "sofa.h"
+#include "sofam.h"
 
 void me_precess(
                  long int mjd, /* (input) modified julian date */
@@ -19,44 +19,35 @@ void me_precess(
                  float *dec  /* (input/output) [deg] dec */
                 ) {
 
-  double JD, H, JD0;
-  double lambda, rho, beta;
-  double lambdaSun, rhoSun, betaSun;
-  double dRA, dDec;
+  double tai1, tai2, tt1, tt2;
+  double dRA, dDec, fRA, fDec, eo;
   
-  /* Get JD from mjd/mpm */
-  JD0 = ((double)mjd) + 2400000.5;     /* ref: http://tycho.usno.navy.mil/mjd.html */
-  H   = ((double)mpm)/(3600.0*1000.0); /* mpm in hours */
-  JD = JD0 + H/24.0; /* days */
-
+  /* Get TAI from mjd/mpm */
+  iauUtctai(mjd+DJM0, mpm/1000.0/86400, &tai1, &tai2);
+  
+  /* Get TT from TAI */
+  iauTaitt(tai1, tai2, &tt1, &tt2);
+  
   /* Convert to radians */
-  dRA = hrrad((double) *ra);
-  dDec = degrad((double) *dec);
+  dRA = (double) *ra * 15 * DD2R;
+  dDec = (double) *dec * DD2R;
   
-  /* Precess to the current epoch from J2000.0 */
-  precess(J2000, JD-MJD0, &dRA, &dDec);
-  
-  /* Convert to ecliptic so that we can do the relativistic deflection */
-  eq_ecl(JD-MJD0, dRA, dDec, &beta, &lambda);
-  
-  /* Locate the Sun */
-  sunpos(JD-MJD0, &lambdaSun, &rhoSun, &betaSun);
-  
-//   /* Apply relativistic deflection */
-//   deflect(JD-MJD0, lambda, beta, lambdaSun, rhoSun, 1e10, &dRA, &dDec);
-  
-  /* Apply nutation */
-  nut_eq(JD-MJD0, &dRA, &dDec);
-  
-  /* Apply aberration */
-  ab_eq(JD-MJD0, lambdaSun, &dRA, &dDec);
+  /* Move into the CIRS */
+  iauAtci13(dRA, dDec, \
+            0.0, 0.0, 0.0, 0.0, \
+            tt1, tt2, \
+            &fRA, &fDec, &eo);
+            
+  /* Swith to equinox based positions */
+  fRA -= eo;
   
   /* Back to floats */
-  *ra = (float) radhr(dRA);
-  *dec = (float) raddeg(dDec);
-
-  return;
+  *ra = (float) fRA * DR2D / 15;
+  *dec = (float) fDec * DR2D;
+  
   } /* me_precess */
 
+// me_precess.c: J. Dowell, UNM, 2022 Oct 6
+//   -- converted to the SOFA library
 // me_precess.c: J. Dowell, UNM, 2015 Aug 17
 //   -- initial version, using me_findjov.c as a starting point 
